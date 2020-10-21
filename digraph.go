1f7f6f8a9f3ebe15e34e23c934baf46527b2f73f
Mikhail Shevchenko <mshev@list.ru>
2020-10-21 22:46:28 +0300

    finished

--- digraph.go
+++ digraph.go
@@ -1,73 +1,55 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 type Digraph = [][]int
 
-var topNameDic = map[int]string{
-	1: "A",
-	2: "B",
-	3: "C",
-	4: "D",
-	5: "E",
-	6: "F",
-	7: "G",
-	8: "H",
+func findCouplingComponents(d Digraph) [][]int {
+	inverted := invert(d)
+	q := makeQueue(inverted)
+	return makeComponentList(d, q)
 }
 
-var testGraph1 = [][]int{
-	[]int{},
-	[]int{2},       //1 A
-	[]int{6, 5, 3}, //2 B
-	[]int{7, 4},    //3 C
-	[]int{8, 3},    //4 D
-	[]int{6, 1},    //5 E
-	[]int{7},       //6 F
-	[]int{6},       //7 G
-	[]int{7, 4},    //8 H
-}
+func makeQueue(inverted Digraph) []int {
+	q := make([]int, 0, len(inverted))
 
-var testGraph2 = [][]int{
-	[]int{},
-	[]int{2},       // 1 A
-	[]int{4},       // 2 B
-	[]int{1},       // 3 C
-	[]int{3},       // 4 D
-	[]int{3, 4, 6}, // 5 E
-	[]int{7},       // 6 F
-	[]int{5},       // 7 G
-}
+	visited := make(map[int]bool)
 
-func printDigraph(digraph Digraph) {
-	str := strings.Builder{}
-	for i := 1; i < len(digraph); i++ {
-		topName, _ := topNameDic[i]
+	for i := range inverted {
+		if _, ok := visited[i]; !ok && i != 0 {
+			stack := &Stack{}
+			detourGraph(inverted, visited, i, stack)
+			for !stack.IsEmpty() {
+				top := stack.Pop()
 
-		str.WriteString(fmt.Sprintf("%s -> ", topName))
-
-		for _, arcs := range digraph[i] {
-			t, _ := topNameDic[arcs]
-			str.WriteString(fmt.Sprintf("%v, ", t))
+				q = append(q, top)
+			}
 		}
-
-		str.WriteString("\n")
 	}
 
-	fmt.Println(str.String())
+	return q
 }
 
-func getTopName(top int) string {
-	if v, ok := topNameDic[top]; ok {
-		return v
-	} else {
-		return ""
+func makeComponentList(d Digraph, q []int) [][]int {
+	res := make([][]int, 0)
+	visited := make(map[int]bool)
+
+	for i := len(q) - 1; i >= 0; i-- {
+
+		if _, ok := visited[q[i]]; !ok {
+			way := &Stack{}
+			detourGraph(d, visited, q[i], way)
+			component := make([]int, 0)
+			for !way.IsEmpty() {
+				component = append(component, way.Pop())
+			}
+
+			res = append(res, component)
+		}
 	}
+
+	return res
 }
 
-func invertDigraph(digraph Digraph) Digraph {
+func invert(digraph Digraph) Digraph {
 	inverted := make([][]int, len(digraph))
 
 	for i := 1; i < len(digraph); i++ {
--- main.go
+++ main.go
@@ -2,53 +2,62 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+var topNameDic = map[int]string{
+	1: "A",
+	2: "B",
+	3: "C",
+	4: "D",
+	5: "E",
+	6: "F",
+	7: "G",
+	8: "H",
+}
+
+var testGraph1 = [][]int{
+	[]int{},
+	[]int{2},       //1 A
+	[]int{6, 5, 3}, //2 B
+	[]int{7, 4},    //3 C
+	[]int{8, 3},    //4 D
+	[]int{6, 1},    //5 E
+	[]int{7},       //6 F
+	[]int{6},       //7 G
+	[]int{7, 4},    //8 H
+}
+
+var testGraph2 = [][]int{
+	[]int{},
+	[]int{2},       // 1 A
+	[]int{4},       // 2 B
+	[]int{1},       // 3 C
+	[]int{3},       // 4 D
+	[]int{6, 3, 4}, // 5 E
+	[]int{7},       // 6 F
+	[]int{5},       // 7 G
+}
+
 func main() {
+	fmt.Printf("Looking for strong coupling components in a digraph:\n\n")
+	printDigraph(testGraph1)
+	res := findCouplingComponents(testGraph1)
+	printCouplingComponents(res)
+
+	fmt.Printf("\nLooking for strong coupling components in a digraph:\n\n")
 	printDigraph(testGraph2)
-	inv := invertDigraph(testGraph2)
-	printDigraph(inv)
+	res = findCouplingComponents(testGraph2)
+	printCouplingComponents(res)
 
-	q := make([]int, 0, len(inv))
+	fmt.Println("\nFinish, press any key")
+	_, _ = fmt.Scanf(" ")
+}
 
-	visited := make(map[int]bool)
-
-	for i, _ := range inv {
-		if _, ok := visited[i]; !ok && i != 0 {
-			part := makeQueue(i, inv, visited)
-			q = append(q, part...)
-			for _, v := range part {
-				visited[v] = true
-			}
-		}
-	}
-
-	fmt.Println(q)
-
-	for _, v := range q {
-		fmt.Println(getTopName(v), v)
-	}
-	fmt.Println("====")
-	res := make([][]int, 0)
-	visited = make(map[int]bool)
-
-	for i := len(q) - 1; i >= 0; i-- {
-
-		if _, ok := visited[q[i]]; !ok {
-			way := &Stack{}
-			detourGraph(testGraph2, visited, q[i], way)
-			component := make([]int, 0)
-			for !way.IsEmpty() {
-				component = append(component, way.Pop())
-			}
-
-			res = append(res, component)
-		}
-	}
-
+func printCouplingComponents(components [][]int) {
 	arrow := "->"
 
-	for idx, component := range res {
+	for idx, component := range components {
 		fmt.Print("Component №", idx+1, ": ")
 		for topIdx, top := range component {
 			if topIdx > 0 {
@@ -60,17 +69,28 @@ func main() {
 	}
 }
 
-func makeQueue(top int, d Digraph, visited map[int]bool) []int {
-	stack := &Stack{}
-	detourGraph(d, visited, top, stack)
+func printDigraph(digraph Digraph) {
+	str := strings.Builder{}
+	for i := 1; i < len(digraph); i++ {
+		topName, _ := topNameDic[i]
 
-	queue := make([]int, 0)
+		str.WriteString(fmt.Sprintf("%s -> ", topName))
 
-	for !stack.IsEmpty() {
-		top := stack.Pop()
+		for _, arcs := range digraph[i] {
+			t, _ := topNameDic[arcs]
+			str.WriteString(fmt.Sprintf("%v, ", t))
+		}
 
-		queue = append(queue, top)
+		str.WriteString("\n")
 	}
 
-	return queue
+	fmt.Println(str.String())
+}
+
+func getTopName(top int) string {
+	if v, ok := topNameDic[top]; ok {
+		return v
+	} else {
+		return ""
+	}
 }
